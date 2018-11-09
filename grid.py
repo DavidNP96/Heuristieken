524d97b6724585129774067ae31f6fec7ba13c70
David Pantophlet <dnpantophlet@gmail.com>
2018-11-09 11:48:46 +0100

    juiste grid

--- grid.py
+++ grid.py
@@ -2,10 +2,24 @@
 
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+
+INPUT_CSV = "wijk1_huizen.csv"
+
+with open('wijk1_huizen.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    xas = []
+    yas = []
+    max_output= []
+    for row in reader:
+        xas.append(int(row["x"]))
+        yas.append(int(row["y"]))
+        max_output.append(row["max. output"])
+    print(xas)
+    print(yas)
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('SmartGrid')
-plt.axis([0, 50, 0, 50])
-plt.grid(True)
+plt.scatter(xas, yas)
 plt.show()
